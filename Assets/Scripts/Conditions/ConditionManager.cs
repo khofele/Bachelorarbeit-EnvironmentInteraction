31c1cc4ab4f0d8e65e66bef4078b18e20626c218
khofele <karen.hofele@gmx.de>
2022-10-06 18:46:16 +0200

    [added] condition manager

---
+++ Assets/Scripts/Conditions/ConditionManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ConditionManager : MonoBehaviour
+{
+    private List<Condition> conditions = new List<Condition>();
+
+    public bool CheckConditions()
+    {
+        foreach (Condition condition in conditions)
+        {
+            if (condition.CheckCondition() == false)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void FillConditionsList()
+    {
+        conditions = GetComponents<Condition>().ToList(); 
+    }
+}
