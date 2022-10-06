26c3f5f7b134e8f19ff5b3bd855dd391da866d42
khofele <karen.hofele@gmx.de>
2022-10-06 18:45:08 +0200

    [added] todo

--- Assets/Scripts/Interactions/ThrowObjectInteraction.cs
+++ Assets/Scripts/Interactions/ThrowObjectInteraction.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ThrowObjectInteraction : Interaction
 {
+    // TODO - regelmäßige Überprüfung des Triggerchecks, Conditionchecks damit Interaktion ausgeführt wird
     public override bool CheckTrigger()
     {
         // TODO implement
