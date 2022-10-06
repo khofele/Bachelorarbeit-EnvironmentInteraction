ea5bc4f604030f0efa70b446d1df7b2302dfce06
khofele <karen.hofele@gmx.de>
2022-10-06 17:46:05 +0200

    [added] throw object interaction script

---
+++ Assets/Scripts/Interactions/ThrowObjectInteraction.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowObjectInteraction : Interaction
+{
+    public override bool CheckTrigger()
+    {
+        // TODO implement
+        return false;
+    }
+
+    public override void ExecuteInteraction()
+    {
+        // TODO implement
+    }
+}
