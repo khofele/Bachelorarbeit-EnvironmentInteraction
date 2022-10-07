26c2fcb392834bbd66bb66bd4d77a31eaccdbf61
khofele <karen.hofele@gmx.de>
2022-10-07 15:48:29 +0200

    [added] code to trigger animations

--- Assets/Scripts/CharController.cs
+++ Assets/Scripts/CharController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class CharController : MonoBehaviour
 {
     [SerializeField] private CharacterController characterController = null;
-    [SerializeField] private float speed = 5f;
+    [SerializeField] private Animator animator = null;
+    [SerializeField] private float speed = 0f;
     [SerializeField] private float gravity = -9.81f * 2;
 
     private Vector3 velocity = Vector3.zero;
@@ -15,6 +16,7 @@ public class CharController : MonoBehaviour
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
 
         Cursor.lockState = CursorLockMode.Locked;
     }
@@ -24,41 +26,52 @@ public class CharController : MonoBehaviour
         float xAxis = Input.GetAxis("Horizontal");
         float zAxis = Input.GetAxis("Vertical");
 
-        moveDirection = transform.right * xAxis + transform.forward * zAxis;
+        moveDirection = new Vector3(xAxis, 0f, zAxis).normalized;
 
-        // Run
-        if(Input.GetKey(KeyCode.LeftShift))
+        // check if button is pressed
+        if(moveDirection.magnitude >= 0.01f)
         {
-            Debug.Log("Running");
-            characterController.Move(moveDirection * (speed*2) * Time.deltaTime);
+
+            if (Input.GetKey(KeyCode.LeftShift) && isCrouching == false)
+            {
+                // Running
+                speed = 10f;
+            }
+            else if (isCrouching == true)
+            {
+                // Crouching
+                speed = 2.5f;
+            }
+            else
+            {
+                // Walking
+                speed = 5f;
+            }
+
             velocity.y += gravity * Time.deltaTime;
+            characterController.Move(moveDirection * speed * Time.deltaTime);
             characterController.Move(velocity * Time.deltaTime);
+
+        }
+        else
+        {
+            speed = 0;
         }
 
-        // Crouch
-        if(Input.GetKeyDown(KeyCode.C) && isCrouching == false)
+
+        // Crouch Toggle
+        if (Input.GetKeyDown(KeyCode.C) && isCrouching == false)
         {
             isCrouching = true;
+            animator.SetBool("isCrouching", true);
         }
         else if (Input.GetKeyDown(KeyCode.C) && isCrouching == true)
         {
             isCrouching = false;
+            animator.SetBool("isCrouching", false);
         }
 
-
-        if(isCrouching == true)
-        {
-            Debug.Log("Crouching");
-            characterController.Move(moveDirection * (speed/2) * Time.deltaTime);
-            velocity.y += gravity * Time.deltaTime;
-            characterController.Move(velocity * Time.deltaTime);
-        }
-        else if(isCrouching == false)
-        {
-            Debug.Log("Walking");
-            characterController.Move(moveDirection * speed * Time.deltaTime);
-            velocity.y += gravity * Time.deltaTime;
-            characterController.Move(velocity * Time.deltaTime);
-        }
+        animator.SetFloat("speed", speed);
+        Debug.Log(speed);
     }
 }
