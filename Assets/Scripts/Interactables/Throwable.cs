0db31c8a8cc8750957f89a8fecc12e9ea701c9bb
khofele <karen.hofele@gmx.de>
2022-10-10 19:19:11 +0200

    [added] mechanic to get grab handle of throwable objects

--- Assets/Scripts/Interactables/Throwable.cs
+++ Assets/Scripts/Interactables/Throwable.cs
@@ -7,4 +7,24 @@ public class Throwable : Interactable
     private Transform grabHandle = null;
 
     public Transform GrabHandle { get => grabHandle; }
+
+    private void Start()
+    {
+        grabHandle = GetGrabHandle();
+        Debug.Log("lol " + grabHandle.name);
+    }
+
+    private Transform GetGrabHandle()
+    {
+        Transform parentTransform = transform;
+
+        foreach(Transform transform in parentTransform)
+        {
+            if(transform.CompareTag("grabHandle"))
+            {
+                return transform;
+            }
+        }
+        return null;
+    }
 }
