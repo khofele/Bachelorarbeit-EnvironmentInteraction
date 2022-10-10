b02db456f9ef149465dd157c682490fca9f899e6
khofele <karen.hofele@gmx.de>
2022-10-10 19:02:29 +0200

    [added] fields and properties, SetLastInteraction method

--- Assets/Scripts/Interactions/InteractionManager.cs
+++ Assets/Scripts/Interactions/InteractionManager.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class InteractionManager : MonoBehaviour
 {
-    private InteractionEnum lastInteraction;
+    private Interaction lastInteraction;
+    private Interaction currentInteraction;
+    private bool isTriggered = false;
     
-    public InteractionEnum LastInteraction { get => lastInteraction; set => lastInteraction = value; }
+    public Interaction LastInteraction { get => lastInteraction; set => lastInteraction = value; }
+    public Interaction CurrentInteraction { get => currentInteraction; set => currentInteraction = value; }
+    public bool IsTriggered { get => isTriggered; set => isTriggered = value; }
+
+    public void SetLastInteraction()
+    {
+        lastInteraction = currentInteraction;
+    }
 }
