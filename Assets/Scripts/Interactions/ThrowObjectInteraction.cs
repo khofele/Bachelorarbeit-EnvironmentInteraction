baaf598db0c62f9bb442ffb8e96ad0efa725eeca
khofele <karen.hofele@gmx.de>
2022-10-10 19:00:41 +0200

    [added] throw method

--- Assets/Scripts/Interactions/ThrowObjectInteraction.cs
+++ Assets/Scripts/Interactions/ThrowObjectInteraction.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class ThrowObjectInteraction : Interaction
 {
-    // TODO - regelmäßige Überprüfung des Triggerchecks, Conditionchecks damit Interaktion ausgeführt wird
-    public override bool CheckTrigger()
+    private CharController charController = null;
+    private IKController iKController = null;
+
+    public override void Start()
     {
-        // TODO implement
-        return false;
+        base.Start();
+        charController = GetComponentInParent<CharController>();
+        iKController = GetComponentInParent<IKController>();
     }
 
     public override void ExecuteInteraction()
     {
-        // TODO implement
+        base.ExecuteInteraction();
+        Debug.Log("Execute ThrowObjectInteraction");
+        //Throw(); ist AnimationEvent
+        // TODO Animation triggern, bisher in IKController
+        interactionManager.CurrentInteraction = this;
+    }
+
+    public void Throw()
+    {
+        if (charController.CurrentInteractable != null)
+        {
+            charController.CurrentInteractable.transform.SetParent(iKController.Environment.transform, true);
+
+            // Throw
+            charController.Animator.SetLookAtWeight(10);
+            Rigidbody rigidBody = charController.CurrentInteractable.GetComponent<Rigidbody>();
+            rigidBody.isKinematic = false;
+            rigidBody.useGravity = true;
+            rigidBody.AddForce(Camera.main.transform.forward * 20 + transform.up * 10, ForceMode.Impulse);
+            // TODO Objekt in Richtung von Gegner werfen --> statt Camera.main.transform.forward anderen Gegenstand anpeilen
+
+            charController.Animator.SetBool("isInteracting", false);
+            iKController.IsIkActive = false;
+        }
     }
 }
