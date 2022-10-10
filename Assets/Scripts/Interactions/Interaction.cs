b3d2dd3868a4422c3332abc501b80826d9470fdc
khofele <karen.hofele@gmx.de>
2022-10-10 19:00:16 +0200

    [modified] interaction parent class [moved] code

--- Assets/Scripts/Interactions/Interaction.cs
+++ Assets/Scripts/Interactions/Interaction.cs
@@ -5,20 +5,48 @@ using UnityEngine;
 [RequireComponent(typeof(ConditionManager))]
 public abstract class Interaction : MonoBehaviour
 {
-    private ConditionManager conditionManager = null;
+    public ConditionManager conditionManager = null;
+    public InteractionManager interactionManager = null;
 
-    public abstract bool CheckTrigger();
-    
-    public abstract void ExecuteInteraction();
-
-    private void Start()
+    public virtual void Start()
     {
         conditionManager = GetComponent<ConditionManager>();
+        interactionManager = GetComponentInParent<InteractionManager>();
+
         conditionManager.FillConditionsList();
+    }
+
+    public virtual void Update()
+    {
+        if (CheckTrigger() == true)
+        {
+            if (CheckConditions() == true)
+            {
+                ExecuteInteraction();
+            }
+        }
+    }
+
+    public virtual bool CheckTrigger()
+    {
+        if (interactionManager.IsTriggered == true)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     public virtual bool CheckConditions()
     {
         return conditionManager.CheckConditions();
+    }
+
+    public virtual void ExecuteInteraction()
+    {
+        interactionManager.SetLastInteraction();
+        interactionManager.IsTriggered = false;
     }
 }
