bc6444536723f9838d7070bd53bafe635e612655
khofele <karen.hofele@gmx.de>
2022-10-12 16:02:58 +0200

    [added] empty hands condition

---
+++ Assets/Scripts/Conditions/EmptyHandsCondition.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmptyHandsCondition : Condition
+{
+    [SerializeField] private Transform rightHand = null;
+    [SerializeField] private Transform leftHand = null;
+
+    private bool isHandsEmpty = false;
+
+    private void Update()
+    {
+        if(rightHand.childCount == 0 && leftHand.childCount == 0)
+        {
+            isHandsEmpty = true;
+        }
+        else
+        {
+            isHandsEmpty = false;
+        }
+    }
+
+    public override bool CheckCondition()
+    {
+        return isHandsEmpty ;
+    }
+
+    public override void ResetCondition()
+    {
+        isHandsEmpty = false;
+    }
+}
