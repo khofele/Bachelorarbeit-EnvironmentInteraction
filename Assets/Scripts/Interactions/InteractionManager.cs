acceef961f27b3f6031a2a0d43f23f7217ae3a5d
khofele <karen.hofele@gmx.de>
2022-10-12 15:59:28 +0200

    [added] default values for fields [added] reset of currentInteraction

--- Assets/Scripts/Interactions/InteractionManager.cs
+++ Assets/Scripts/Interactions/InteractionManager.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 
 public class InteractionManager : MonoBehaviour
 {
-    private Interaction lastInteraction;
-    private Interaction currentInteraction;
+    private Interaction lastInteraction = null;
+    private Interaction currentInteraction = null;
     private bool isTriggered = false;
-    
+
     public Interaction LastInteraction { get => lastInteraction; set => lastInteraction = value; }
     public Interaction CurrentInteraction { get => currentInteraction; set => currentInteraction = value; }
     public bool IsTriggered { get => isTriggered; set => isTriggered = value; }
@@ -16,5 +16,6 @@ public class InteractionManager : MonoBehaviour
     public void SetLastInteraction()
     {
         lastInteraction = currentInteraction;
+        currentInteraction = null;
     }
 }
