63589b3166e4177452a48095fd991d110269c3c6
khofele <karen.hofele@gmx.de>
2022-10-17 17:51:24 +0200

    [added] fields, snap-mechanic, todo

--- Assets/Scripts/Interactions/LeanOnWallInteraction.cs
+++ Assets/Scripts/Interactions/LeanOnWallInteraction.cs
@@ -4,24 +4,58 @@ using UnityEngine;
 
 public class LeanOnWallInteraction : Interaction
 {
-    private void OnTriggerEnter(Collider other)
+    [SerializeField] private Collider wallCollider = null;
+    [SerializeField] private BoxCollider walkCollider = null;
+    [SerializeField] private Collider playerCollider = null;
+    private float snapDistance = 1f;
+    private Vector3 offset;
+
+    // TODO Collider von Wand bekommen
+    // TODO Wand muss drei Collider haben --> Leanable-Skript
+
+    private void LeanOnWall()
     {
-        
+        Vector3 playerClosestPoint = playerCollider.ClosestPoint(wallCollider.transform.position);
+        Vector3 wallClosestPoint = wallCollider.ClosestPoint(playerClosestPoint);
+        offset = wallClosestPoint - playerClosestPoint;
+
+        if(offset.magnitude < snapDistance)
+        {
+            charController.transform.position += offset;
+        }
+    }
+
+    public override void Start()
+    {
+        base.Start();
+        Physics.IgnoreCollision(playerCollider, walkCollider);
+        fittingInteractable = typeof(Leanable);
+    }
+
+    public override void Update()
+    {
+        base.Update();
+        if (isInteracting == true)
+        {
+            LeanOnWall();
+                
+            if (offset.magnitude > snapDistance || charController.IsCrouching == false)
+            {
+                isInteracting = false;
+            }
+        }
     }
 
     public override void ExecuteInteraction()
     {
-        base.ExecuteInteraction();
         Debug.Log("Execute LeanOnWallInteraction");
-    }
-
-    private void LeanOnWall()
-    {
-
+        interactionManager.CurrentInteraction = this;
+        isInteracting = true;
+        base.ExecuteInteraction();
     }
 
     public override void ResetInteraction()
     {
-
+        // TODO Wandcollider resetten
     }
 }
