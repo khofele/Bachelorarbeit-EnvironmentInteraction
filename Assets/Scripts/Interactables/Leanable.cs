0b70e4a71e3e4f2a3a9b7520dc63469cf9905779
khofele <karen.hofele@gmx.de>
2022-10-19 14:09:08 +0200

    [renamed] field [added] validate method

--- Assets/Scripts/Interactables/Leanable.cs
+++ Assets/Scripts/Interactables/Leanable.cs
@@ -5,14 +5,33 @@ using UnityEngine;
 
 public class Leanable : Interactable
 {
+    private Collider snapCollider = null;
     private Collider objectCollider = null;
-    public Collider ObjectCollider { get => objectCollider; }
+    public Collider SnapCollider { get => snapCollider; }
 
     public override void Validate()
     {
         base.Validate();
         GetComponent<Rigidbody>().isKinematic = true;
 
-        objectCollider = GetComponent<Collider>();
+        if (transform.childCount == 0)
+        {
+            GameObject emptyGameObject = new GameObject();
+            emptyGameObject.transform.SetParent(transform);
+
+            emptyGameObject.AddComponent(objectCollider.GetType());
+            emptyGameObject.transform.localPosition = Vector3.zero;
+            snapCollider = emptyGameObject.GetComponent<Collider>();
+
+        }
+        else if (GetComponentInChildren<Collider>() == null)
+        {
+            transform.GetChild(0).gameObject.AddComponent(objectCollider.GetType());
+            snapCollider = transform.GetChild(0).GetComponent<Collider>();
+        }
+        else
+        {
+            snapCollider = transform.GetChild(0).GetComponent<Collider>(); 
+        }
     }
 }
