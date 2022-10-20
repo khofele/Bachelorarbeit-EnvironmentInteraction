485b66278425873c27ace1f3be1cc3b01882052e
khofele <karen.hofele@gmx.de>
2022-10-20 13:46:21 +0200

    [added] edge lean script

---
+++ Assets/Scripts/Interactions/EdgeLeanInteraction.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EdgeLeanInteraction : LeanInteraction
+{
+    // TODO implementieren
+    public override void ExecuteAnimation()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void StopAnimation()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override bool TerminationCondition()
+    {
+        throw new System.NotImplementedException();
+    }
+}
