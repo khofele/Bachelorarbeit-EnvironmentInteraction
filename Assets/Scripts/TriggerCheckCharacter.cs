35e590092214e1aa45c331f52b71dd5fb16cbd39
khofele <karen.hofele@gmx.de>
2022-10-26 18:06:14 +0200

    [renamed] script

--- Assets/Scripts/TriggerCheckCharacter.cs
+++ Assets/Scripts/TriggerCheckCharacter.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TriggerCheck : MonoBehaviour
+public class TriggerCheckCharacter : MonoBehaviour
 {
     private InteractionManager interactionManager = null;
     private InteractableManager interactableManager = null;
