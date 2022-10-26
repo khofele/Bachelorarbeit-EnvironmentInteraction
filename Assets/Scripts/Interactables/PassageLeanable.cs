9ded7aef177fc64c1b11ca9666487d0e5948effe
khofele <karen.hofele@gmx.de>
2022-10-26 18:06:38 +0200

    [implemented] passage leanable script

--- Assets/Scripts/Interactables/PassageLeanable.cs
+++ Assets/Scripts/Interactables/PassageLeanable.cs
@@ -4,5 +4,49 @@ using UnityEngine;
 
 public class PassageLeanable : FixedLeanable
 {
-   
+    private BoxCollider oppositeWallCollider = null;
+
+    public BoxCollider OppositeWall { get => oppositeWallCollider; }
+
+    // TODO Snap- und Resettransforms automatisch adden
+
+    // TODO schön machen
+    public override void Validate()
+    {
+        base.Validate();
+
+        // TODO Möglichkeit finden zweite Wand zu detecten und ggf. hinzuzufügen, sonst per Skript prüfen (Skript anhängen, dass Cube definiert) oder Prefab
+        BoxCollider[] children = transform.GetComponentsInChildren<BoxCollider>();
+        foreach (BoxCollider child in children)
+        {
+            if(child.CompareTag("OppositeWall"))
+            {
+                oppositeWallCollider = child;
+            }
+        }
+
+
+        foreach(BoxCollider child in children)
+        {
+            if(child.gameObject.transform.CompareTag("OppositeWall") == false)
+            {
+                snapCollider = child;
+            }
+        }
+
+        if(oppositeWallCollider == null)
+        {
+            Debug.LogError("No opposite wall found!");
+        }
+
+        if(snapCollider == null)
+        {
+            GameObject emptyGameObject = new GameObject("Collider");
+            emptyGameObject.transform.SetParent(transform);
+
+            emptyGameObject.AddComponent(objectCollider.GetType());
+            emptyGameObject.transform.localPosition = Vector3.zero;
+            snapCollider = emptyGameObject.GetComponent<Collider>();
+        }
+    }
 }
