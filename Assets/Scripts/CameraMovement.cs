094c95b73a5b9570429e3aadc2f4f5878e7d33c6
khofele <karen.hofele@gmx.de>
2022-10-26 18:11:59 +0200

    [added] new camera [added] camera movement script

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private CinemachineFreeLook freeLookCamera = null;
+    [SerializeField] private CinemachineVirtualCamera leanCamera = null;
+    [SerializeField] private CinemachineVirtualCamera topDownCamera = null;
+    private InteractionManager interactionManager = null;
+    private List<CinemachineVirtualCamera> virtualCameras = new List<CinemachineVirtualCamera>();
+
+    private void Start()
+    {
+        interactionManager = GetComponentInChildren<InteractionManager>();
+
+        virtualCameras.Add(leanCamera);
+        virtualCameras.Add(topDownCamera);
+    }
+
+    private void Update()
+    {
+        if (interactionManager.IsLeaningOnEdge == false && interactionManager.IsCrouchingLeaning == false && interactionManager.IsStandingLeaning == false && interactionManager.IsLeaningInPassage == false)
+        {
+            freeLookCamera.enabled = true;
+        }
+        else if (interactionManager.IsLeaningInPassage == true)
+        {
+            EnableVirtualCamera(topDownCamera);
+        }
+        else
+        {
+            EnableVirtualCamera(leanCamera);
+        }
+    }
+
+    private void EnableVirtualCamera(CinemachineVirtualCamera camera)
+    {
+        foreach(CinemachineVirtualCamera vCam in virtualCameras)
+        {
+            if(vCam == camera)
+            {
+                vCam.enabled = true;
+            }
+            else
+            {
+                vCam.enabled = false;
+            }
+        }
+        freeLookCamera.enabled = false;
+    }
+}
