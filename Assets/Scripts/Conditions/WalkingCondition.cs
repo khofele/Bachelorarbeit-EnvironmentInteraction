4189c0d00f22f0c82d7bd98b9fc4b6967fcce45d
khofele <karen.hofele@gmx.de>
2022-10-27 17:17:18 +0200

    [added] walking condition script

---
+++ Assets/Scripts/Conditions/WalkingCondition.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkingCondition : Condition
+{
+    public override bool CheckCondition()
+    {
+        // TODO implement
+        return false;
+    }
+}
