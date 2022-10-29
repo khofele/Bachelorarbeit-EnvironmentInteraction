d14ba8d416e9ca5430cd248f2b7bd94b2c156af8
khofele <karen.hofele@gmx.de>
2022-10-29 19:54:21 +0200

    [added] script for jump over obstacle interaction

---
+++ Assets/Scripts/Interactions/JumpOverObstacleInteraction.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpOverObstacleInteraction : Interaction
+{
+    protected override void ResetInteraction()
+    {
+        // TODO implement
+    }
+
+    protected override void SetMatchingInteractable()
+    {
+        // TODO implement
+    }
+}
