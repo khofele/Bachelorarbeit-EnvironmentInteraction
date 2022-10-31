8a174300d52abf801cd1a9f9b94fc224b020d9d7
khofele <karen.hofele@gmx.de>
2022-10-31 19:02:11 +0100

    [added] leaning bools

--- Assets/Scripts/Interactions/InteractionManager.cs
+++ Assets/Scripts/Interactions/InteractionManager.cs
@@ -10,19 +10,47 @@ public class InteractionManager : MonoBehaviour
     private bool isInteractionTriggered = false;
 
     // Interaction bools
+    private bool isLeaning = false;
+    private bool isFixedLeaning = false;
     private bool isLeaningOnEdge = false;
     private bool isCrouchingLeaning = false;
     private bool isStandingLeaning = false;
     private bool isLeaningInPassage = false;
+    private bool isJumping = false;
 
+    public bool IsLeaning { get => isLeaning; }
+    public bool IsFixedLeaning { get => isFixedLeaning; }
     public bool IsLeaningOnEdge { get => isLeaningOnEdge; set => isLeaningOnEdge = value; }
     public bool IsCrouchingLeaning { get => isCrouchingLeaning; set => isCrouchingLeaning = value; }
     public bool IsStandingLeaning { get => isStandingLeaning; set => isStandingLeaning = value; }
     public bool IsLeaningInPassage { get => isLeaningInPassage; set => isLeaningInPassage = value; }
+    public bool IsJumping { get => isJumping; set => isJumping = value; }
 
     public Interaction LastInteraction { get => lastInteraction; set => lastInteraction = value; }
     public Interaction CurrentInteraction { get => currentInteraction; set => currentInteraction = value; }
     public bool IsInteractionTriggered { get => isInteractionTriggered; set => isInteractionTriggered = value; }
+
+
+    private void Update()
+    {
+        if(isLeaningOnEdge == false && isCrouchingLeaning == false && isStandingLeaning == false && isLeaningInPassage == false)
+        {
+            isLeaning = false;
+        }
+        else
+        {
+            isLeaning = true;
+        }
+
+        if(isLeaningOnEdge == true || isLeaningInPassage == true)
+        {
+            isFixedLeaning = true;
+        }
+        else
+        {
+            isFixedLeaning = false;
+        }
+    }
 
     public void SetLastInteraction()
     {
