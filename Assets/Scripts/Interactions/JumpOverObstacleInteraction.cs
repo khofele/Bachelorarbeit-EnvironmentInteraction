9bd90d45af64a57abfa9e353c430c5e8fdf48248
khofele <karen.hofele@gmx.de>
2022-10-31 19:02:48 +0100

    [implemented] jump mechanic

--- Assets/Scripts/Interactions/JumpOverObstacleInteraction.cs
+++ Assets/Scripts/Interactions/JumpOverObstacleInteraction.cs
@@ -6,11 +6,40 @@ public class JumpOverObstacleInteraction : Interaction
 {
     protected override void ResetInteraction()
     {
-        // TODO implement
+        StartCoroutine(WaitAndReset());
     }
 
     protected override void SetMatchingInteractable()
     {
-        // TODO implement
+        matchingInteractable = typeof(Jumpable);
+    }
+
+    protected override void ExecuteInteraction()
+    {
+        base.ExecuteInteraction();
+        Jump();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if(interactionManager.IsJumping == true)
+        {
+            charController.transform.position += (new Vector3(Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z).normalized) * Time.deltaTime * 2;
+        }
+    }
+
+    private void Jump()
+    {
+        animationManager.ExecuteJumpAnimation();
+        interactionManager.IsJumping = true;
+    }
+
+    private IEnumerator WaitAndReset()
+    {
+        yield return new WaitForSeconds(1f);
+        interactionManager.IsJumping = false;
+        isInteractionRunning = false;
     }
 }
