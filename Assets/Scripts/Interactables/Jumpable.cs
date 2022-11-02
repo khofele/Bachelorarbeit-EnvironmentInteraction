225eb62439d101dc6dd6ce6c631362503d12d046
khofele <karen.hofele@gmx.de>
2022-11-02 15:29:14 +0100

    [added] property and todo

--- Assets/Scripts/Interactables/Jumpable.cs
+++ Assets/Scripts/Interactables/Jumpable.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class Jumpable : Interactable
 {
+    private Collider jumpCollider = null;
+
+    public Collider JumpCollider { get => jumpCollider; }
+
     protected override void Start()
     {
         base.Start();
         GetComponent<Rigidbody>().isKinematic = true;
+        jumpCollider = transform.GetChild(0).GetComponent<Collider>(); // --> Validate-Ticket
     }
+
+    // TODO Validate, dass Höhe und Breite stimmt! --> Validate-Ticket
 }
