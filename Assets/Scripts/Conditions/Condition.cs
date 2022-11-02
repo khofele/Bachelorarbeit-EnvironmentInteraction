1be0c3c0c37daa90e22562b5af90b31086c340b7
khofele <karen.hofele@gmx.de>
2022-11-02 15:28:53 +0100

    [added] field to condition parent class

--- Assets/Scripts/Conditions/Condition.cs
+++ Assets/Scripts/Conditions/Condition.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public abstract class Condition : MonoBehaviour
 {
     protected CharController charController = null;
+    protected InteractionManager interactionManager = null;
 
     protected virtual void Start()
     {
         charController = GetComponentInParent<CharController>();
+        interactionManager = FindObjectOfType<InteractionManager>();
     }
 
     public abstract bool CheckCondition();
