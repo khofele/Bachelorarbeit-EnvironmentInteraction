e28e0fa959bd8c37288f4332fdc34bd670b100c0
khofele <karen.hofele@gmx.de>
2022-11-02 15:29:27 +0100

    [removed] old code

--- Assets/Scripts/Conditions/InteractionAlreadyRunningCondition.cs
+++ Assets/Scripts/Conditions/InteractionAlreadyRunningCondition.cs
@@ -4,16 +4,6 @@ using UnityEngine;
 
 public class InteractionAlreadyRunningCondition : Condition
 {
-    // DEBUG
-    // TODO falls behalten schön machen
-    private InteractionManager interactionManager = null;
-
-    protected override void Start()
-    {
-        base.Start();
-        interactionManager = FindObjectOfType<InteractionManager>();
-    }
-
     public override bool CheckCondition()
     {
         if(interactionManager.CurrentInteraction != null)
