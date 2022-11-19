dec2c49e2bb7f9a9517ba627fb38f46a1556acd3
khofele <karen.hofele@gmx.de>
2022-11-19 16:13:07 +0100

    [modified] condition parent

--- Assets/Scripts/Conditions/Abstract/Condition.cs
+++ Assets/Scripts/Conditions/Abstract/Condition.cs
@@ -6,11 +6,13 @@ public abstract class Condition : MonoBehaviour
 {
     protected CharController charController = null;
     protected InteractionManager interactionManager = null;
+    protected InteractableManager interactableManager = null;
 
     protected virtual void Start()
     {
         charController = GetComponentInParent<CharController>();
         interactionManager = FindObjectOfType<InteractionManager>();
+        interactableManager = FindObjectOfType<InteractableManager>();
     }
 
     public abstract bool CheckCondition();
