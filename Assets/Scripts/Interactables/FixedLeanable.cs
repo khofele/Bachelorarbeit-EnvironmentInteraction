e14583980af19d48fb02074cbdbde9cd18d9e1a7
khofele <karen.hofele@gmx.de>
2022-12-08 19:45:13 +0100

    [removed] old code

--- Assets/Scripts/Interactables/FixedLeanable.cs
+++ Assets/Scripts/Interactables/FixedLeanable.cs
@@ -32,54 +32,6 @@ public class FixedLeanable : Leanable
         GetTransforms();
     }
 
-    protected override void ModifyTrigger()
-    {
-        // Trigger 1
-        if (GetComponent<BoxCollider>() != null)
-        {
-            triggerOne = GetComponent<BoxCollider>();
-            triggerOne.isTrigger = true;
-            triggerOne.size = new Vector3(0.01f, 1f, 2.4f);
-            triggerOne.center = new Vector3(0.5f, 0f, -0.7f);
-        }
-        else
-        {
-            triggerOne = gameObject.AddComponent<BoxCollider>();
-            triggerOne.isTrigger = true;
-            triggerOne.size = new Vector3(0.01f, 1f, 2.4f);
-            triggerOne.center = new Vector3(0.5f, 0f, -0.7f);
-        }
-
-        // Trigger 2
-        BoxCollider[] boxColliders = GetComponents<BoxCollider>();
-
-        if (boxColliders.Length <= 1)
-        {
-            triggerTwo = gameObject.AddComponent<BoxCollider>();
-            triggerTwo.isTrigger = true;
-            triggerTwo.size = new Vector3(0.1f, 1f, 2.4f);
-            triggerTwo.center = new Vector3(-0.45f, 0f, -0.7f);
-        }
-        else
-        {
-            foreach(BoxCollider boxCollider in boxColliders)
-            {
-                if (boxCollider != triggerOne)
-                {
-                    triggerTwo = boxCollider;
-                    triggerTwo.isTrigger = true;
-                    triggerTwo.size = new Vector3(0.1f, 1f, 2.4f);
-                    triggerTwo.center = new Vector3(-0.45f, 0f, -0.7f);
-                }
-            }
-        }
-
-        if (triggerOne == null || triggerTwo == null)
-        {
-            Debug.LogError("Trigger not found!");
-        }
-    }
-
     private void GetTransforms()
     {
         Transform[] children = GetComponentsInChildren<Transform>();
--- Assets/Scripts/Interactables/Leanable.cs
+++ Assets/Scripts/Interactables/Leanable.cs
@@ -6,39 +6,35 @@ using UnityEngine;
 public class Leanable : Interactable
 {
     protected Collider snapCollider = null;
-    protected Collider objectCollider = null;
+    //protected Collider objectCollider = null;
     public Collider SnapCollider { get => snapCollider; }
 
     protected override void Validate()
     {
         base.Validate();
 
-        objectCollider = GetComponent<Collider>();
+        //objectCollider = GetComponent<Collider>();
         GetComponent<Rigidbody>().isKinematic = true;
 
-        ModifyTrigger();
+        //ModifyTrigger();
         GetSnapCollider();
     }
 
-    protected virtual void ModifyTrigger()
-    {
-        if (objectCollider.GetType() == typeof(BoxCollider))
-        {
-            BoxCollider boxCollider = (BoxCollider)objectCollider;
-            boxCollider.size = new Vector3(1.2f, 1f, 2f);
-        }
-        else if (objectCollider.GetType() == typeof(SphereCollider))
-        {
-            SphereCollider sphereCollider = (SphereCollider)objectCollider;
-            sphereCollider.radius = 1f;
-        }
-        else if (objectCollider.GetType() == typeof(CapsuleCollider))
-        {
-            CapsuleCollider capsuleCollider = (CapsuleCollider)objectCollider;
-            capsuleCollider.radius = 1f;
-            capsuleCollider.height = 3f;
-        }
-    }
+    //protected virtual void ModifyTrigger()
+    //{
+    //    if (objectCollider.GetType() == typeof(BoxCollider))
+    //    {
+    //        BoxCollider boxCollider = (BoxCollider)objectCollider;
+    //    }
+    //    else if (objectCollider.GetType() == typeof(SphereCollider))
+    //    {
+    //        SphereCollider sphereCollider = (SphereCollider)objectCollider;
+    //    }
+    //    else if (objectCollider.GetType() == typeof(CapsuleCollider))
+    //    {
+    //        CapsuleCollider capsuleCollider = (CapsuleCollider)objectCollider;
+    //    }
+    //}
 
     protected virtual void GetSnapCollider()
     {
