bad37b445604d08ede140ef0fb21493c7fbeeb49
khofele <karen.hofele@gmx.de>
2022-12-12 16:52:34 +0100

    [removed] old code

--- Assets/Scripts/Interactables/Leanable.cs
+++ Assets/Scripts/Interactables/Leanable.cs
@@ -6,35 +6,15 @@ using UnityEngine;
 public class Leanable : Interactable
 {
     protected Collider snapCollider = null;
-    //protected Collider objectCollider = null;
+
     public Collider SnapCollider { get => snapCollider; }
 
     protected override void Validate()
     {
         base.Validate();
 
-        //objectCollider = GetComponent<Collider>();
-        GetComponent<Rigidbody>().isKinematic = true;
-
-        //ModifyTrigger();
         GetSnapCollider();
     }
-
-    //protected virtual void ModifyTrigger()
-    //{
-    //    if (objectCollider.GetType() == typeof(BoxCollider))
-    //    {
-    //        BoxCollider boxCollider = (BoxCollider)objectCollider;
-    //    }
-    //    else if (objectCollider.GetType() == typeof(SphereCollider))
-    //    {
-    //        SphereCollider sphereCollider = (SphereCollider)objectCollider;
-    //    }
-    //    else if (objectCollider.GetType() == typeof(CapsuleCollider))
-    //    {
-    //        CapsuleCollider capsuleCollider = (CapsuleCollider)objectCollider;
-    //    }
-    //}
 
     protected virtual void GetSnapCollider()
     {
