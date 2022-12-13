ba739ed0d6de19198b85f9e9207c4ac8b7cafe7d
khofele <karen.hofele@gmx.de>
2022-12-13 18:46:00 +0100

    [added] interruptible interaction base

---
+++ Assets/Scripts/Interactions/Abstract/InterruptibleInteraction.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class InterruptibleInteraction : Interaction
+{
+    [SerializeField] protected List<Interaction> interruptInteractions = new List<Interaction>();
+
+    protected bool CheckInterruptInteractionsConditions()
+    {
+        foreach(Interaction interaction in interruptInteractions)
+        {
+            if(interaction.CheckConditions() == true)
+            {
+                interaction.IsTriggeredByInterrupibleInteraction = true;
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
